39e43c65e93bbb60ee7b9a7a79691d53db209279
SuNetVerseWolk <volk333333333@gmail.com>
2024-04-19 11:49:25 +0300

    bit fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Materials from './pages/Materials'
 import Login from './pages/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Routes>
       <Route path="materials" element={<Materials />} >
