5cc1726aa1247084cbf82860f96ac2c903268361
ki <145863668+kirillBanchenco@users.noreply.github.com>
2024-04-23 09:38:29 +0300

    demo maket

--- frontend/src/pages/Materials.jsx
+++ frontend/src/pages/Materials.jsx
@@ -1,13 +1,39 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import styles from 'styles/presStyle.module.css'
 
 const Materials = () => {
   return (
-    <div>
-      <h1>Materials</h1>
+    <div className={styles.presContainer}>
+      <header>
+        <h1>Презентация</h1>
+
+        <div>
+          <p>Лу Луин Лоун</p>
+
+          <button><img src="/exit.png" alt="..." /></button>
+        </div>
+      </header>
       
       <div>
-        <Outlet />
+        <div className={styles.asideBar}>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+
+          <button>+</button>
+        </div>
+        <div>
+          <Outlet />
+        </div>
       </div>
     </div>
   )
